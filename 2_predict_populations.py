278a1f5ab425b9b3a813dfcc00ad9ef0c2690306
Bruce Chappell <brucechappelliii@gmail.com>
2021-05-15 19:53:54 +0200

    syntax fix

--- 2_predict_populations.py
+++ 2_predict_populations.py
@@ -3,28 +3,28 @@ import h5py
 from networkUtils.atmosphereFunctions import predict_populations
 
 if __name__ == "__main__":
-    
+
     SIM_NUM  = 's525'
     SIM_NAME = 'qs006023'
-    MODEL = 'ColMass_3x3'
+    MODEL = 'SunnyNet'
     MODEL_NAME = 'model_name'
     TRAIN_FILE = 'train_file.hdf5'
-    
+
     # file to dataset NN was trained on
     TRAIN_DATA = f'path/{TRAIN_FILE}'
-                                                                                               
+
     # file of prepped data from 1_build_solving_set.py
-    TEST_DATA = f'path/{SIM_NAME}_{SIM_NUM}.hdf5' 
-                                                                                                
-    # save path                                                                                           
+    TEST_DATA = f'path/{SIM_NAME}_{SIM_NUM}.hdf5'
+
+    # save path
     SAVE = f'path/{MODEL_NAME}/{SIM_NAME}_{SIM_NUM}.hdf5'
-    
+
     folder1 = '/'.join(SAVE.split('/')[:-1])
     try:
         assert os.path.exists(folder1) == True
     except AssertionError:
         os.mkdir(folder1)
-        
+
     assert os.path.exists(SAVE) == False, "dont want to overwrite just incase. check filenames"
 
     ## predict atmos configuration ##
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         'alpha': 0.2,
         'output_XY': 256,
     }
-       
+
     final, z, cmass_mean, cmass_scale = predict_populations(TEST_DATA, TRAIN_DATA, pred_config)
 
     print('Exponentiate')
@@ -51,4 +51,3 @@ if __name__ == "__main__":
         dset1.attrs['z'] = z
         dset1.attrs['cmass_mean'] = cmass_mean
         dset1.attrs['cmass_scale'] = cmass_scale
-    
